346736b288c78881d6506e503959067b96fa27f8
hanbing426 <m18798375374@163.com>
2025-07-22 18:36:46 +0800

    优化a

--- src/main/java/com/epoch/controller/Result.java
+++ src/main/java/com/epoch/controller/Result.java
@@ -7,6 +7,20 @@ public class Result {
     private Integer code;
     private Object data;
     private String message;
+    private Long total;  // 添加 total 字段，表示总记录数
+    private int totalPages;
+    private int pageNumber;
+    private int pageSize;
+
+    public Result(Integer code, Object data, String message, Long total, int totalPages, int pageNumber, int pageSize) {
+        this.code = code;
+        this.data = data;
+        this.message = message;
+        this.total = total;
+        this.totalPages = totalPages;
+        this.pageNumber = pageNumber;
+        this.pageSize = pageSize;
+    }
 
     public Result(Integer code, Object data, String message) {
         this.code = code;
